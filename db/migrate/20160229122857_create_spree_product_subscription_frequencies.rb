2dd9d1467f2bb4723c570b7daaf60418e5299b5f
TakamizawaNoriaki <hardreggaecafe@gmail.com>
2019-12-10 16:04:30 +0900

    Add index name

--- db/migrate/20160229122857_create_spree_product_subscription_frequencies.rb
+++ db/migrate/20160229122857_create_spree_product_subscription_frequencies.rb
@@ -2,7 +2,7 @@ class CreateSpreeProductSubscriptionFrequencies < ActiveRecord::Migration[5.2]
   def change
     create_table :spree_product_subscription_frequencies do |t|
       t.references :product, index: true
-      t.references :subscription_frequency
+      t.references :subscription_frequency, index: { name: 'frequencies_index' }
     end
   end
 end

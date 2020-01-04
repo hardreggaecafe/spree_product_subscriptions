618faab21d9e8a690726e6b5ab54f5384aefbf1f
TakamizawaNoriaki <hardreggaecafe@gmail.com>
2020-01-04 11:16:33 +0900

    add_occurence_possible_column_to_spree_subscriptions

--- db/migrate/20180508100649_add_occurence_possible_column_to_spree_subscriptions.rb
+++ db/migrate/20180508100649_add_occurence_possible_column_to_spree_subscriptions.rb
@@ -1,4 +1,4 @@
-class AddOccurencePossibleColumnToSpreeSubscriptions < ActiveRecord::Migration[4.2]
+class AddOccurencePossibleColumnToSpreeSubscriptions < ActiveRecord::Migration[5.2]
   def change
     add_column :spree_subscriptions, :next_occurrence_possible, :boolean, default: true
   end

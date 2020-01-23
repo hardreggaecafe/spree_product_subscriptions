d1f8187f038acba72e78fc773dff079f8b284fbf
TakamizawaNoriaki <hardreggaecafe@gmail.com>
2020-01-24 00:36:56 +0900

    change user_controller

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -1,11 +1,16 @@
 Spree::UsersController.class_eval do
 
+  before_action :check_login_user, only: :show
   before_action :load_subscriptions, only: :show
 
   private
 
+    def check_login_user
+      render_404 and return if current_user.nil?
+    end
+
     def load_subscriptions
-      @orders = @user.orders.complete.order(completed_at: :desc)
+      @orders = current_user.orders.complete.order(completed_at: :desc)
       @subscriptions = Spree::Subscription.active.order(created_at: :desc).with_parent_orders(@orders)
     end
 
